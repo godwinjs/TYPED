608f490aaf0e7baf46574dd5d0f3ac639f33a1f5
GodwinJS <godwinikechukwu.dev@gmail.com>
2023-04-27 09:19:02 +0100

    added items

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
+import {Row, Col} from 'react-bootstrap'
+import storeItems from '../data/items.json'
+import { StoreItems } from '../components/StoreItems'
+
 export function Store(){
-    return <h1>store</h1>
+    return <>
+        <h1>store</h1>
+        <Row md={2} xs={1} lg={3} className='g-3'>
+            {storeItems.map(item => {
+                return <Col key={item.id}><StoreItems {...item} /></Col>
+            })}
+        </Row>
+    </>
 }
